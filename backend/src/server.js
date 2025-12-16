384b84bba7d68b2e57ca12f37814e737f0a1efc6
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2025-12-16 11:20:10 +0500

    committed changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,13 @@ import app from "../src/app.js";
 import { connectDB } from "../src/db/connectDB.js";
 const PORT = process.env.PORT || 3000;
 
-connectDB()
-  .then(() =>
-    console.log("Connected established with Postgres SQL successfully")
-  )
-  .catch((err) => {
-    throw new Error("Some error occured while connecting to db", err);
-  });
+// connectDB()
+//   .then(() =>
+//     console.log("Connected established with Postgres SQL successfully")
+//   )
+//   .catch((err) => {
+//     throw new Error("Some error occured while connecting to db", err);
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
